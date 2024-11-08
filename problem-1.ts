ac0664324201ca3643c9aed601187bdea37651d1
Naaf Izz Tushaar <nafijul52.71@gmail.com>
2024-11-08 16:06:23 +0600

    something

---
+++ problem-1.ts
@@ -0,0 +1,7 @@
+ console.log("something"); 
+ //* git init 
+ //* git add. 
+ //* git commit -m "something" 
+ //* git branch -M main 
+ //* git remote add origin YOUR_REPO_LINK 
+ //* git push u origin main -f
